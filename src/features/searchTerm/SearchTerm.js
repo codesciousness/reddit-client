bc42777cdbf56b81a6fd056f4815e39a428e7dcd
codesciousness <codesciousness@gmail.com>
2022-11-10 22:41:23 -0600

    Edit the margin values in the SearchTerm and Post components.

--- src/features/searchTerm/SearchTerm.js
+++ src/features/searchTerm/SearchTerm.js
@@ -6,17 +6,20 @@ import { setSearchTerm, clearSearchTerm, selectSearchTerm } from './searchTermSl
 const SearchTerm = () => {
     const searchTerm = useSelector(selectSearchTerm);
     const dispatch = useDispatch();
+
     const handleChange = ({ target }) => {
         dispatch(setSearchTerm(target.value));
-    }
+    };
+
     const handleClick = () => {
         dispatch(clearSearchTerm());
-    }
+    };
+    
     return (
         <form className="SearchTerm">
-            <i className="fas fa-search fa-lg SearchTerm__icon"></i>
+            <span className="SearchTerm__icon"><i className="fas fa-search fa-lg"></i></span>
             <input className="SearchTerm__input" placeholder="Search..." onChange={handleChange} value={searchTerm} />
-            {searchTerm && <i className="fas fa-times fa-lg SearchTerm__icon" onClick={handleClick} role="button"></i>}
+            {searchTerm && <span className="SearchTerm__icon button"><i className="fas fa-times fa-lg" onClick={handleClick} role="button"></i></span>}
         </form>
     );
 }
